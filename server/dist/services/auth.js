c68512f73e6337fd3de1f91fbc6c8f3b44f48a56
joel <jroney002@gmail.com>
2024-10-27 16:57:52 -0500

    set up a change to connection because db not working

---
+++ server/dist/services/auth.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import { GraphQLError } from 'graphql';
+import dotenv from 'dotenv';
+dotenv.config();
+export const authenticateToken = ({ req }) => {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+    if (req.headers.authorization) {
+        token = token.split(' ').pop().trim();
+    }
+    if (!token) {
+        return req;
+    }
+    try {
+        const userData = jwt.verify(token, process.env.JWT_SECRET_KEY || '', { maxAge: '1hr' });
+        req.user = userData;
+    }
+    catch (err) {
+        console.log('Invalid token.');
+    }
+};
+export const signToken = (username, email, _id) => {
+    const payload = { username, email, _id };
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+    return jwt.sign({ data: payload }, secretKey, { expiresIn: '1h' });
+};
+export class AuthenticationError extends GraphQLError {
+    constructor(message) {
+        super(message, undefined, undefined, undefined, ['UNAUTHENTICATED']);
+        Object.defineProperty(this, 'name', { value: 'AuthenticationError' });
+    }
+}
+;
