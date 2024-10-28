076edc8c0b26571d24c102fb25734e5320b00a5f
joel <jroney002@gmail.com>
2024-10-28 17:13:11 -0500

    set up app.tsx on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,41 @@
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 import { Outlet } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: 'graphql',
+});
+
+const authLink = setContext(({ headers }) => { // Why error? removed _, as first argument
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
